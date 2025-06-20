46c5dadf2540b43d17e8496ee29e1f80a0b08681
sryang <sarang628@gmail.com>
2025-06-20 14:11:46 +0800

    update

---
+++ RestaurantServiceModule.kt
@@ -0,0 +1,22 @@
+package com.sarang.restaurantinfotest.di.restauarnt_info_di
+
+import com.sarang.torang.GetRestaurantInfoUseCase
+import com.sarang.torang.RestaurantInfoData
+import com.sarang.torang.api.ApiRestaurant
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+class RestaurantServiceModule {
+    @Provides
+    fun providesGetRestaurantInfoUseCase(apiRestaurant: ApiRestaurant): GetRestaurantInfoUseCase {
+        return object : GetRestaurantInfoUseCase {
+            override suspend fun invoke(restaurantId: Int): RestaurantInfoData {
+                return apiRestaurant.getRestaurantDetail(restaurantId).toRestaurantInfoData()
+            }
+        }
+    }
+}
