8ac1fab9582cefcdf30e6550035d32e9e1ad21e7
sryang <sarang628@gmail.com>
2025-08-10 16:33:56 +0800

    add default value

--- DataConverter.kt
+++ DataConverter.kt
@@ -7,20 +7,20 @@ import com.sarang.torang.data.RestaurantDetail
 
 fun RestaurantDetail.toRestaurantInfoData(): RestaurantInfoData {
     return RestaurantInfoData(
-        foodType = this.restaurant.restaurantType,
+        foodType = this.restaurant.restaurantType ?: "",
         open = "영업 중",
         close = "오후 9:00에 영업 종료",
-        address = this.restaurant.address,
-        webSite = this.restaurant.website,
-        tel = this.restaurant.tel,
-        name = this.restaurant.restaurantName,
+        address = this.restaurant.address ?: "",
+        webSite = this.restaurant.website ?: "",
+        tel = this.restaurant.tel ?: "",
+        name = this.restaurant.restaurantName ?: "",
         imageUrl = BuildConfig.RESTAURANT_IMAGE_SERVER_URL + this.restaurant.imgUrl1,
         hoursOfOperation = this.hoursOfOperations.map { it.toHoursOfOperation() },
-        rating = this.restaurant.rating,
-        reviewCount = this.restaurant.reviewCount,
-        price = this.restaurant.prices,
-        lon = this.restaurant.lon,
-        lat = this.restaurant.lat
+        rating = this.restaurant.rating ?: 0f,
+        reviewCount = this.restaurant.reviewCount ?: 0,
+        price = this.restaurant.prices ?: "",
+        lon = this.restaurant.lon ?: 0.0,
+        lat = this.restaurant.lat ?: 0.0
     )
 }
 
