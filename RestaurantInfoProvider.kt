7316add0e8977a258be3e298ad9c4313961fbb70
sryang <sarang628@gmail.com>
2025-09-08 17:53:39 +0800

    add RestaurantInfo

--- RestaurantInfoProvider.kt
+++ RestaurantInfoProvider.kt
@@ -1,18 +1,27 @@
 package com.sarang.torang.di.restauarnt_info_di
 
 import androidx.annotation.RequiresPermission
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.sarang.torang.RestaurantInfo
 import com.sarang.torang.RestaurantInfoScreen
+import com.sarang.torang.RestaurantInfoViewModel
 import com.sryang.library.compose.workflow.BestPracticeViewModel
 
+@Composable
 @RequiresPermission(anyOf = ["android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"])
-fun restaurantInfo(): RestaurantInfo =
+fun restaurantInfo(viewModel : RestaurantInfoViewModel = hiltViewModel()): RestaurantInfo =
     { restaurantId, onLocation, onWeb, onCall ->
+        val uiState = viewModel.uiState
+        LaunchedEffect(restaurantId) { viewModel.fetchRestaurantInfo1(restaurantId) }
         RequestLocationBox { currentLocation, onRequestLocation ->
             PermissionBox(BestPracticeViewModel(), onRequestLocation) { isGranted, request ->
-                RestaurantInfoScreen(
-                    currentLocation = currentLocation,
-                    restaurantId = restaurantId,
+                LaunchedEffect(currentLocation) {
+                    currentLocation?.let { viewModel.setCurrentLocation(it.first, it.second) }
+                }
+                RestaurantInfo(
+                    uiState = uiState,
                     isLocationPermissionGranted = isGranted,
                     onLocation = onLocation,
                     onWeb = onWeb,
