195650954f7c01f4e15b959821360d8fb24697e9
sryang <sarang628@gmail.com>
2025-09-08 17:53:18 +0800

    rename RestaurantInfoScreenProvider.kt

---
+++ RestaurantInfoScreenProvider.kt
@@ -0,0 +1,24 @@
+package com.sarang.torang.di.restauarnt_info_di
+
+import androidx.annotation.RequiresPermission
+import com.sarang.torang.RestaurantInfo
+import com.sarang.torang.RestaurantInfoScreen
+import com.sryang.library.compose.workflow.BestPracticeViewModel
+
+@RequiresPermission(anyOf = ["android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"])
+fun restaurantInfoScreen(): RestaurantInfo =
+    { restaurantId, onLocation, onWeb, onCall ->
+        RequestLocationBox { currentLocation, onRequestLocation ->
+            PermissionBox(BestPracticeViewModel(), onRequestLocation) { isGranted, request ->
+                RestaurantInfoScreen(
+                    currentLocation = currentLocation,
+                    restaurantId = restaurantId,
+                    isLocationPermissionGranted = isGranted,
+                    onLocation = onLocation,
+                    onWeb = onWeb,
+                    onCall = onCall,
+                    onRequestPermission = { request() }
+                )
+            }
+        }
+    }
